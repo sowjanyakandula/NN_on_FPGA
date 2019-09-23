1a06d17e8127b5ab3634c4ca6ae094a403eca1c0
Shivamnz <44774442+Shivamnz@users.noreply.github.com>
2019-09-23 14:02:25 +0530

    Add files via upload

--- read_data.v
+++ read_data.v
@@ -8,16 +8,17 @@ module read_data(
     
     reg [9:0] addr;
     reg [7:0] dout;
-    always@(posedge clk)
+    wire [7:0] dout;
+    always@(posedge clk) 
+    begin
     if(rst == 1'b1)
         addr <= 10'b0000000000;
     else
-    begin
-        /*blk_mem_gen_0 your_instance_name (
-        .clka(clk),    // input wire clka
-        .addra(addr),  // input wire [9 : 0] addra
-        .douta(dout)  // output wire [7 : 0] douta
-        );*/
         addr <= addr + 1'b1;
     end
+    blk_mem_gen_0 your_instance_name(
+    .clka(clk),    // input wire clka
+    .addra(addr),  // input wire [9 : 0] addra
+    .douta(dout)  // output wire [7 : 0] douta
+    );
 endmodule
