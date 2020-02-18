254f5442e193d77c470654e00a3b40787e154f9f
Shivamnz <44774442+Shivamnz@users.noreply.github.com>
2020-02-18 14:17:17 +0530

    To create the case structure for sigmoid block
    The code converts decimal numbers after decimal point into binary

---
+++ Coe_file_and_other_file_codes/sigmoid_lut.m
@@ -0,0 +1,15 @@
+%Script to write verilog code of sigmoid 
+clear all
+close all
+clc
+
+fid = fopen('D:\NN_on_FPGA\sigmoid_implementation.txt', 'wt');
+
+fprintf(fid, sprintf('always @(in)\n'));
+fprintf(fid, sprintf('begin\n'));
+fprintf(fid, sprintf('\tcase(in)\n'));
+for i= 0:((2^11)-1)
+    fprintf(fid, sprintf('\t\t11''b%s: out <= 8''b%s;\n', dec2bin(i,11),dec2bin((1/(1+exp(-i/256))*256),8)));
+end
+fprintf(fid, sprintf('\tendcase\n'));
+fprintf(fid, sprintf('end'));
